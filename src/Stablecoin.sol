5a33b2cd5e4f1a8391428f445dd9f5d450db7a8d
Ashy5000 <ashydashy5000@gmail.com>
2024-10-19 21:11:46 -0700

    Bugfix attempt

--- src/Stablecoin.sol
+++ src/Stablecoin.sol
@@ -1,3 +1,21 @@
 // SPDX-License-Identifier: GPL-3
+pragma solidity ^0.8.20;
 
-contract Stablecoin {}
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Stablecoin is ERC20 {
+    IERC20 public usdc = IERC20(0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8);
+
+    constructor() ERC20("Unistable", "UUSD") {}
+
+    function wrap(uint256 amount) public {
+        usdc.transferFrom(msg.sender, address(this), amount);
+        _mint(msg.sender, amount);
+    }
+
+    function unwrap(uint256 amount) public {
+        _burn(msg.sender, amount);
+        usdc.transfer(msg.sender, amount);
+    }
+}
